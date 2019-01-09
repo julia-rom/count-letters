c27ab61560504b8105e85d75e62a2edf7ed6e23b
Julia <juliaromanowski@gmail.com>
2019-01-09 19:47:25 +0000

    got it working

--- count-letters2.js
+++ count-letters2.js
@@ -1,5 +1,33 @@
 var input = "lighthouse in the house";
 
-function letterPosition(letters) {
+function countLetters(string) {
+    var small = input.replace(/\s/gi, "").toLowerCase();
+
+    var obj = {}
+
+    for (let i = 0; i < small.length; i++) {
+        if (!obj.hasOwnProperty(small[i])) {
+            obj[small[i]] = [];
+        }
+        obj[small[i]].push(i);
+    }
+    return (obj)
 
 }
+
+console.log(countLetters(input));
+
+
+
+// {
+//     l: 1,
+//     i: [1, 9]
+//     g: 1,
+//     h: [4, 5]
+//     t: 2,
+//     o: 2,
+//     u: 2,
+//     s: 2,
+//     e: 3,
+//     n: 1,
+//   }
